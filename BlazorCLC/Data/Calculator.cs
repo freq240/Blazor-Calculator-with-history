ac35566a658bfcddbaaca4f4f12d13ec3fe2e169
Kostyuk Andrey <freq240@gmail.com>
2021-09-08 16:28:58 +0300

    Add project files.

---
+++ BlazorCLC/Data/Calculator.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCLC.Data
+{
+    public class Calculator
+    {
+        public string valueFirst { get; set; }
+        public string valueSecond { get; set; }
+
+        public double digitFirst { get; set; }
+        public double digitSecond { get; set; }
+
+        public int count { get; set; }
+
+        // digits
+        public void buttonClick(string text)
+        {
+            if (valueFirst != null)
+            {
+                // checking on two dots in a row
+                if (valueFirst.Contains(".") && text == ".")
+                {
+
+                }
+                else
+                {
+                    valueFirst += text;
+                }
+            }
+            else
+            {
+                valueFirst += text;
+            }
+
+        }
+
+        // operation
+        public void buttonClick(int count)
+        {
+            try
+            {
+                digitFirst = double.Parse(valueFirst);
+                valueFirst = "";
+                this.count = count;
+                switch (count)
+                {
+                    case 1:
+                        valueSecond = digitFirst.ToString() + " + ";
+                        break;
+                    case 2:
+                        valueSecond = digitFirst.ToString() + " - ";
+                        break;
+                    case 3:
+                        valueSecond = digitFirst.ToString() + " * ";
+                        break;
+                    case 4:
+                        valueSecond = digitFirst.ToString() + " / ";
+                        break;
+                    case 5:
+                        valueSecond = digitFirst.ToString() + " % ";
+                        break;
+                    case 6:
+                        Calculate();
+                        break;
+                    case 7:
+                        Calculate();
+                        break;
+                    case 8:
+                        Calculate();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        public void Calculate()
+        {
+            this.valueSecond = "";
+            try
+            {
+                switch (count)
+                {
+                    case 1:
+                        digitSecond = digitFirst + double.Parse(valueFirst);
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 2:
+                        digitSecond = digitFirst - double.Parse(valueFirst);
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 3:
+                        digitSecond = digitFirst * double.Parse(valueFirst);
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 4:
+                        digitSecond = digitFirst / double.Parse(valueFirst);
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 5:
+                        digitSecond = digitFirst % double.Parse(valueFirst);
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 6:
+                        digitSecond = 1 / digitFirst;
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 7:
+                        digitSecond = Math.Pow(digitFirst, 2);
+                        valueFirst = digitSecond.ToString();
+                        break;
+                    case 8:
+                        digitSecond = Math.Sqrt(digitFirst);
+                        valueFirst = digitSecond.ToString();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        public void Clear(int x)
+        {
+
+            switch (x)
+            {
+                case 0:
+
+                    // Delete and clear all
+                    valueFirst = "";
+                    valueSecond = "";
+                    break;
+                case 1:
+
+                    // Deleting last symbol
+
+                    if (valueFirst != null)
+                    {
+                        int length = valueFirst.Length - 1;
+                        string line = valueFirst;
+                        valueFirst = "";
+                        for (int i = 0; i < length; i++)
+                        {
+                            valueFirst += line[i];
+                        }
+                    }
+                    break;
+
+
+                default:
+                    break;
+            }
+
+        }
+
+    }
+}
---
+++ BlazorCLC/Data/CalculatorCI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCLC.Data
+{
+    public class CalculatorCI
+    {
+        public string StartSum { get; set; }
+        public string PercentsInYear { get; set; }
+        public string TimesAddSum { get; set; }
+        public string Years { get; set; }
+        public bool flag { get; set; } = false;
+
+        public bool IncorrectInputCI { get; set; } = false;
+        public double valueCI { get; set; }
+
+
+
+        public void ShowCompoundInterestMenu()
+        {
+            if (flag)
+            {
+                this.flag = false;
+            }
+            else
+            {
+                this.flag = true;
+            }
+        }
+
+        public void CalculateCI(string StartSum, string PercentInYear, string Times, string Years)
+        {
+            try
+            {
+                double P = Convert.ToDouble(StartSum);
+                double i = Convert.ToDouble(PercentInYear);
+                int n = Convert.ToInt32(Times);
+                int t = Convert.ToInt32(Years);
+
+                this.IncorrectInputCI = false;
+
+                this.valueCI = P * Math.Pow((1 + (i / 100.0) / n), (n * t));
+            }
+            catch
+            {
+                this.IncorrectInputCI = true;
+            }
+        }
+    }
+}
